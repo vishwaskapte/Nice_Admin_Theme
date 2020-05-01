5fed88347b9a4dde85613cf7256c17276aa6c11d
Vishwas Kapte <vishwasvkapte@outlook.com>
2020-05-01 11:20:23 +0530

    Add project files.

---
+++ Nice_Admin_Theme/Controllers/NiceAdminController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nice_Admin_Theme.Controllers
+{
+    public class NiceAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nice_Admin_Theme/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nice_Admin_Theme.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
